7a260922ce88c37edb0f0a89227ab088023ab73a
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-03 23:28:19 +0100

    Creating the Chat list Items

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/ChatListItem/Index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem/>
+      <ChatListItem/>
+      <ChatListItem/>
+      <ChatListItem/>
+      <ChatListItem/>
+      <ChatListItem/>
+    
       <StatusBar style="auto" />
     </View>
   );
